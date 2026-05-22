94dfd4dfadf751d52dca47c2d05daddd610ccc25
Diego-Capu <diegoospino487@gmail.com>
2026-05-21 19:44:26 -0500

    Please enter the commit message for your changes. Lines starting

--- notas.js
+++ notas.js
@@ -1,27 +1,40 @@
 const inputNota = document.getElementById("inputNota");
 const listaNotas = document.getElementById("listaNotas");
 const btnAgregar = document.getElementById("btnAgregar");
-
+let notas = [];
 function agregarNota() {
     const textclean = inputNota.value.trim();
     if (textclean === "") {
         alert("Ingrese una nota")
         return;
     }
-    const li = document.createElement("li");
-    const span = document.createElement("span");
-    span.textContent = textclean;
-    const boton = document.createElement("button");
-    boton.textContent = "Eliminar"
-    boton.addEventListener("click", function () {
-        boton.parentElement.remove();
-    });
-    li.appendChild(span)
-    li.appendChild(boton)
-    listaNotas.appendChild(li)
+    notas.push(textclean);
+    localStorage.setItem("notas", JSON.stringify(notas))
+    renderizarNotas();
     inputNota.value = ""
     inputNota.focus()
-
 }
-
-btnAgregar.addEventListener("click", agregarNota);   
+function renderizarNotas() {
+    listaNotas.innerHTML = ""
+    notas.forEach((element, index) => {
+        const li = document.createElement("li");
+        const span = document.createElement("span");
+        span.textContent = element;
+        const boton = document.createElement("button");
+        boton.textContent = "Eliminar"
+        boton.addEventListener("click", function () {
+            notas.splice(index, 1);
+            localStorage.setItem("notas", JSON.stringify(notas));
+            renderizarNotas();
+        });
+        li.appendChild(span)
+        li.appendChild(boton)
+        listaNotas.appendChild(li)
+    });
+}
+btnAgregar.addEventListener("click", agregarNota);
+if (localStorage.getItem("notas")){
+    notas = JSON.parse(localStorage.getItem("notas"));
+    console.log("notas cargadas:",notas.length)
+}
+renderizarNotas();
